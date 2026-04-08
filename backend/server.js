174bc92e9acf798a3c300e036eb9a58d84b8e2cc
Malaya Kumar Pradhan <malayakumarpradhan1507@gmail.com>
2026-04-08 16:26:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true 
+  }
 });
 
 // Safe Database Initialization
